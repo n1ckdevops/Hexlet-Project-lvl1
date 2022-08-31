761fe911a163ee9b85c4f49555d4da02b071f085
Nickilas Kuchinsky <muyyzdali@gmail.com>
2022-08-31 22:55:42 +0300

    still working

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,27 +2,44 @@ import prompt
 from random import randint
 
 
-random_number = randint(1, 100)
-
-
-correct_answer = "yes"
-wrong_answer = "no"
-#string = ''
-
-
 def is_brain_even():
-    print('Answer "yes" if the number is even, otherwise answer "no"')
-    question = f"Question: {random_number}"
-    print(question)
-    answer = prompt.string("Your answer: ")
-    print(answer)
-    if random_number % 2 == 0 and answer == correct_answer:
-        print('Correct!')
-    else:
-        print(f"""'{answer}' is wrong answer ;(. Correct answer was 'no'.
-Let's try again,
-        """)
+    correct_answer = "yes"
+    wrong_answer = "no"
+    i = 0
+    while i < 3:
+
+        random_number = randint(1, 50)
+        print('Answer "yes" if the number is even, otherwise answer "no"')
+        question = f"Question: {random_number}"
+        print(question)
+        answer = prompt.string("Your answer: ")
+        if answer == 'yes' and random_number % 2 == 0:
+            print('Correct!')
+
+        elif answer == 'no' and random_number % 2 != 0:
+            print("Correct!")
+
+        elif answer == 'yes' and random_number % 2 != 0:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {wrong_answer}.\n"
+                  f"Let's try again,\n"
+                  f"                ")
+
+        elif answer == 'no' and random_number % 2 == 0:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {correct_answer}.\n"
+                  f"Let's try again,\n"
+                  f"                ")
+
+        elif answer and random_number % 2 == 0:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {correct_answer}.\n"
+                  f"Let's try again,\n"
+                  f"                ")
+
+        elif answer and random_number % 2 != 0:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {wrong_answer}.\n"
+                  f"Let's try again,\n"
+                  f"                ")
+
+    print("Congratulations, Bill!")
 
 
 is_brain_even()
-  #
