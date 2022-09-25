e566e4845e26c83598c94f394bd00dd0353d14b7
Nickilas Kuchinsky <muyyzdali@gmail.com>
2022-09-26 02:04:36 +0300

    fix bugs

--- brain_games/games/game.py
+++ brain_games/games/game.py
@@ -38,7 +38,7 @@ def is_even():
     i = 0
     while i != 3:
         random_number = randint(1, 50)
-        print("Question: " + str(random_number))
+        print(f"Question: {random_number}")
         answer = prompt.string("Your answer: ")
         nothing = "yes" if answer == "no" else "no"
         if answer == 'yes' and random_number % 2 == 0:
